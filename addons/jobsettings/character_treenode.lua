fe185cebaac54d4a3bfd77712387d61397fc0d09
arosecra <aaron.rosecrans@gmail.com>
2021-03-31 23:44:58 -0500

    refactored methods

--- addons/jobsettings/character_treenode.lua
+++ addons/jobsettings/character_treenode.lua
@@ -5,6 +5,7 @@ local libs2imgui = require('org_github_arosecra/imgui');
 local macros_configuration = require('org_github_arosecra/macros/macros_configuration');
 local macros_runner = require('org_github_arosecra/macros/macrorunner');
 local imgui = require('imgui');
+local setting = require('setting');
 local character_treenode = {};
 
 
@@ -12,36 +13,48 @@ character_treenode.draw = function(runtime_config, name, mainjob, subjob)
     if imgui.TreeNode(name .. ' (' .. mainjob .. ')') then
                     
         local settings = libs2config.get_string_table(addon.name, mainjob, "Settings");
-        settings:each(function(setting, setting_index)
-            local setting_name = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting .. '.Name');
-            local setting_values = libs2config.get_string_table(addon.name, "Settings", setting .. '.Values');
-            local selected_value = ""
-            if runtime_config[name] ~= nil and runtime_config[name][setting_name] ~= nil then
-                selected_value = setting_values[runtime_config[name][setting_name]]
-            end
+        settings:each(function(setting_array_name, setting_index)
+            local setting_name = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting_array_name .. '.Name');
+            local setting_values = libs2config.get_string_table(addon.name, "Settings", setting_array_name .. '.Values');
 
-            if imgui.BeginCombo(setting_name, selected_value, 0) then
-                for j=1,#setting_values do
-                    local selected = runtime_config[name][setting_name] == j;
-                    imgui.PushID(name .. setting_name .. setting_values[j]);
-                    if imgui.Selectable(setting_values[j], selected) then
-                        runtime_config[name][setting_name] = j;
-                        local macro_id = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting .. '.' .. setting_values[j] .. '.Macro');
-                        local macro = macros_configuration.get_macro_by_id(macro_id)
-                        macros_runner.run_macro(macro);
-                    end
-
-                    if selected then
-                        imgui.SetItemDefaultFocus();
-                    end
-                    imgui.PopID();
+            if setting_name ~= nil and setting_values ~= nil then
+                
+                local selected_value = ""
+                if runtime_config[name] ~= nil and runtime_config[name][setting_name] ~= nil then
+                    selected_value = setting_values[runtime_config[name][setting_name]]
                 end
-                imgui.EndCombo();
+
+                character_treenode.draw_combo(runtime_config, name, setting_name, setting_values, selected_value);
             end
 
         end);
     end
 end
 
+character_treenode.draw_combo = function(runtime_config, name, setting_name, setting_values, selected_value)
+
+    if imgui.BeginCombo(setting_name, selected_value, 0) then
+        for j=1,#setting_values do
+            character_treenode.draw_select(runtime_config, name, setting_name, j, setting_values[j])
+        end
+        imgui.EndCombo();
+    end
+
+end
+
+
+character_treenode.draw_select = function(runtime_config, name, setting_name, index, setting_value)
+    local selected = runtime_config[name][setting_name] == index;
+    imgui.PushID(name .. setting_name .. setting_value);
+    if imgui.Selectable(setting_value, selected) then
+        runtime_config[name][setting_name] = index;
+        setting.run_macro(setting_name, setting_value);
+    end
+
+    if selected then
+        imgui.SetItemDefaultFocus();
+    end
+    imgui.PopID();
+end
 
 return character_treenode;
--- addons/jobsettings/setting.lua
+++ addons/jobsettings/setting.lua
@@ -1,6 +1,8 @@
 
 
 local libs2config = require('org_github_arosecra/config');
+local macros_configuration = require('org_github_arosecra/macros/macros_configuration');
+local macros_runner = require('org_github_arosecra/macros/macrorunner');
 local setting = {};
 
 setting.default_settings_for_char = function(runtime_config, name, mainjob, subjob) 
@@ -9,13 +11,14 @@ setting.default_settings_for_char = function(runtime_config, name, mainjob, subj
         runtime_config[name].MainJob = mainjob;
         runtime_config[name].SubJob = subjob;
         local settings = libs2config.get_string_table(addon.name, mainjob, "Settings");
-        settings:each(function(setting, setting_index)
-            local setting_name = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting .. '.Name');
-            local setting_values = libs2config.get_string_table(addon.name, "Settings", setting .. '.Values');
-            local default = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting .. '.Default');
+        settings:each(function(setting_array_name, setting_index)
+            local setting_name = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting_array_name .. '.Name');
+            local setting_values = libs2config.get_string_table(addon.name, "Settings", setting_array_name .. '.Values');
+            local default = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting_array_name .. '.Default');
             for j=0,#setting_values do
                 if setting_values[j] == default then
                     runtime_config[name][setting_name] = j;
+                    setting.run_macro(setting_name, setting_values[j]);
                 end
             end
             
@@ -23,5 +26,12 @@ setting.default_settings_for_char = function(runtime_config, name, mainjob, subj
     end
 end
 
+setting.run_macro = function(setting_name, setting_value)
+    local macro_id = AshitaCore:GetConfigurationManager():GetString(addon.name, "Settings", setting_name .. '.' .. setting_value .. '.Macro');
+    if macro_id ~= nil then
+        local macro = macros_configuration.get_macro_by_id(macro_id)
+        macros_runner.run_macro(macro);
+    end
+end
 
 return setting;
